fb7898a784360a5e2796bcc9aa8842637be55d92
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-09-11 17:11:56 -0300

    example

---
+++ src/go.js
@@ -0,0 +1,23 @@
+import { parser } from "./parser.js"
+
+const example = `VAR_J = 2;
+C1;
+FORK ROT_C3;
+C2;
+JOIN VAR_J, ROT_C4, QUIT;
+
+ROT_C3:
+  C3;
+  JOIN VAR_J, ROT_C4, QUIT;
+
+ROT_C4:
+  C4;
+`;
+
+const tree = parser.parse(example);
+
+console.log(tree);
+for (const c in tree.children) {
+	console.log(tree.children[c]);
+}
+console.log(tree.toString());
